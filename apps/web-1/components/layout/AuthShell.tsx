bbb990ad19468242222deed3a54dc27ec373b302
UGing265 <duongquocthai368@gmail.com>
2026-08-20 19:54:35 +0700

    refactor: remove logo

--- apps/web-1/components/layout/AuthShell.tsx
+++ apps/web-1/components/layout/AuthShell.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Link from "next/link";
 import ThemeToggler from "../ui/ThemeToggler";
-import Logo from "../ui/Logo";
 
 interface AuthShellProps {
   children: React.ReactNode;
@@ -17,13 +15,6 @@ export default function AuthShell({ children }: AuthShellProps) {
 
       <div className="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:px-8">
         <div className="mx-auto w-full max-w-md">
-          {/* Brand Logo */}
-          <div className="flex justify-center mb-6">
-            <Link href="/" className="inline-block hover:opacity-90 transition-opacity">
-              <Logo height={38} />
-            </Link>
-          </div>
-
           {/* Form Content */}
           <div className="bg-surface-app py-8 px-5 border border-border-app rounded-xl shadow-xs sm:px-10">
             {children}
